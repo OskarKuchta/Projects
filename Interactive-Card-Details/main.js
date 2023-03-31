9d683ca75d2777b6e16893749e9f0a5c61d05c24
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-03-31 20:37:24 +0200

    Some functions to change card values

--- Interactive-Card-Details/main.js
+++ Interactive-Card-Details/main.js
@@ -1,4 +1,24 @@
 const showCvc = document.querySelector("#show-cvc");
 const cardNumber = document.querySelector("#numbers-top");
-const
-console.log(cardNumber);
+const owner = document.querySelector("#owner");
+const cardExp = document.querySelector("#card-expires");
+const ownerInput = document.querySelector("#firstlastName");
+const cardNumberInput = document.querySelector("#cardNumber");
+const expDateInput = document.querySelector("#exp-month");
+const expYearInput = document.querySelector("#exp-year");
+const cvcInput = document.querySelector("#cvc-num");
+const submit = document.querySelector(".submit-btn");
+
+
+ownerInput.addEventListener("input", () => {
+    owner.innerHTML = (ownerInput.value).toUpperCase();
+});
+
+cvcInput.addEventListener("input", () => {
+    if(cvcInput.length < 4){
+        cvcInput.value = showCvc.innerHTML;
+    }
+    else {
+        showCvc.innerHTML = (cvcInput.value).slice(0,3);
+    }
+});
