69ffad9b801fce0cecffc27112aee2414c605e29
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-03 18:43:49 +0200

    Add error text

--- Interactive-Card-Details/main.js
+++ Interactive-Card-Details/main.js
@@ -10,12 +10,13 @@ const cvcInput = document.querySelector("#cvc-num");
 const submit = document.querySelector(".submit-btn");
 const content = document.querySelector("#content");
 const submitCard = document.querySelector(".submitted");
+
 ownerInput.addEventListener("input", () => {
     owner.innerHTML = (ownerInput.value).toUpperCase();
 });
 cardNumberInput.addEventListener("input", () => {
     let splitNumber = cardNumberInput.value;
-        cardNumber.innerHTML = ([...splitNumber].map((d, i) => (i) % 4 == 0 ? ' ' + d : d).join('').trim());
+    cardNumber.innerHTML = ([...splitNumber].map((d, i) => (i) % 4 == 0 ? ' ' + d : d).join('').trim());
 });
 expMonthInput.addEventListener("input", () => {
     cardExp.innerHTML = expMonthInput.value + " / " + expYearInput.value;
@@ -24,6 +25,9 @@ expYearInput.addEventListener("input", () => {
     cardExp.innerHTML = expMonthInput.value + " / " + expYearInput.value;
 })
 cvcInput.addEventListener("input", () => {
+    if(cvcInput.value == ""){
+        
+    }
     showCvc.innerHTML = cvcInput.value;
 });
 submit.addEventListener("click", (event) => {
