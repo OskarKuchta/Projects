71a750dccd3f6a7ebfa51fa3c9b4d43bf589b012
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-23 16:45:02 +0200

    Add components and advices in json format

---
+++ advice-generator/src/Top.js
@@ -0,0 +1,7 @@
+function Top() {
+
+}
+
+
+
+export default Top
--- advice-generator/src/index.js
+++ advice-generator/src/index.js
@@ -1,3 +1,19 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./style.css";
+import Top from "./Top";
+import { Bottom } from "./Bottom";
+
+function App() {
+    return (
+        <main>
+            <Top />
+            <Bottom />
+        </main>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <App />
+)
