82221b22914e2d647dda67c58ad3a651909da0a6
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-05-13 13:57:57 +0200

    Fix milion if else check to one loop

--- Interactive-Rating-Component/code/index.js
+++ Interactive-Rating-Component/code/index.js
@@ -1,64 +1,52 @@
 let firstPage = document.getElementById("first");
 let secondPage = document.getElementById("second");
-let firstBtn = document.getElementById("firstchoice");
-let secondBtn = document.getElementById("secondchoice");
-let thirdBtn = document.getElementById("thirdchoice");
-let fourthBtn = document.getElementById("fourthchoice");
-let fifthBtn = document.getElementById("fifthchoice");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
 let submitBtn = document.getElementById("submitBtn");
 let text = document.getElementById("text");
 let darkBtn = document.querySelector(".toggle");
 let light = document.querySelector(".light");
 let dark = document.querySelector(".dark");
-checkFirst = () => {
-    firstBtn.checked = true;
-  }
-  checkSecond = () => {
-    secondBtn.checked = true;
-  }
-  checkThird = () => {
-    thirdBtn.checked = true;
-  }
-  checkFourth = () => {
-    fourthBtn.checked = true;
-  }
-  checkFifth = () => {
-    fifthBtn.checked = true;
-  }
-submitChoice = () => {
-    if(firstBtn.checked){
-        text.innerHTML= "You selected 1 out of 5";
-    }
-    else if(secondBtn.checked){
-        text.innerHTML= "You selected 2 out of 5";
-    }
-    else if(thirdBtn.checked){
-        text.innerHTML= "You selected 3 out of 5";
-    }
-    else if(fourthBtn.checked){
-        text.innerHTML= "You selected 4 out of 5";
-    }
-    else if(fifthBtn.checked){
-        text.innerHTML= "You selected 5 out of 5";
-    }
-    else {
-        text.innerHTML = "Oops, you forgot select any rate";
-    }
-    firstPage.style.display = "none";
-    secondPage.style.display = "block";
-
+let btnValue;
+let backBtn = document.querySelector("#back-button");
+const checkBtn = (btn) => {
+  btn.checked = true;
 }
+
+for (let i = 1; i <= 5; i++) {
+  const btn = document.getElementById(`btn${i}`);
+  btn.addEventListener("click", () => {
+    checkBtn(btn);
+    btnValue = btn.value;
+  })
+}
+
+submitBtn.addEventListener("click", () => {
+  btnValue != undefined ?
+    text.innerHTML = `You gave ${btnValue == 1 ? btnValue + " star" : btnValue + " stars"}, thank for rate!`
+    : text.innerHTML = "You forgot to leave a rate!";
+  firstPage.style.display = "none";
+  secondPage.style.display = "block";
+})
+
+backBtn.addEventListener("click", () => {
+  location.reload();
+})
+
 lightMode = () => {
-  if(dark.style.display === "none") {
-  light.style.display = "none";
-  dark.style.display = "inline-block";
+  if (dark.style.display === "none") {
+    light.style.display = "none";
+    dark.style.display = "inline-block";
   }
   else {
-  dark.style.display = "none";
-  light.style.display = "inline-block";
+    dark.style.display = "none";
+    light.style.display = "inline-block";
   }
   document.body.classList.toggle("lightmode");
-  if(dark.style.display === "inline-block") {
+  if (dark.style.display === "inline-block") {
     darkBtn.style.backgroundColor = "rgba(195, 206, 193, 0.693)";
   }
   else {
